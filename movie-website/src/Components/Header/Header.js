f9b459ea24f803b2e91e2294c2d938efe0dfecf7
Katherine Allen <katherineallen067@gmail.com>
2020-10-18 19:09:58 -0400

    sunday night functionailty complete

--- movie-website/src/Components/Header/Header.js
+++ movie-website/src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import '../../styles/component-styles/Header.scss';
 const Header = () => {
 	return (
 		<div className="header-row">
-			<h1 className="title">The SHOPPIES</h1>
+			<h1 className="title">The Movie Nominations</h1>
 		</div>
 	);
 };
