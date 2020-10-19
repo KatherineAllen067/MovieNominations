b14d23aa77aec5006b40ac80a9f190b04cfcb915
Katherine Allen <katherineallen067@gmail.com>
2020-10-19 11:18:32 -0400

    background colors changed to gradient

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import './App.scss';
 import Header from './Components/Header/Header.js';
 import Main from './Components/Main/Main.js';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-      <BrowserRouter>
+    <div className="app">
       <Header />
-      <Switch>
-        <Route path="/" component={Main} />
-        <Route path="/:movieid" component={Main} />
-      </Switch>
-      </BrowserRouter>
+      <Main />
+    </div>
   );
 }
 
